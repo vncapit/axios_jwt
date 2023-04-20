72b59d269d7b6646927fb095e84236dc11eb61a0
cap <vncapit@gmail.com>
2023-04-20 22:18:47 +0800

    first commit

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <b-form @submit="onSubmit">
+            <b-form-group
+                id="username-group"
+                label="Username:"
+                label-for="username"
+                description="Please input OA username."
+            >
+                <b-form-input
+                    id="username-input"
+                    v-model="form.username"
+                    type="text"
+                    placeholder="Username"
+                    required
+                >
+                </b-form-input>
+            </b-form-group>
+            <b-form-group
+                id="password-group"
+                label="Password:"
+                label-for="password"
+                description="Please input OA password."
+            >
+                <b-form-input
+                    id="password-input"
+                    v-model="form.password"
+                    type="password"
+                    placeholder="Password"
+                    required
+                >
+                </b-form-input>
+            </b-form-group>
+            <b-button type="submit" variant="primary">Login</b-button>
+            <b-button type="button" variant="danger">Cancel</b-button>
+        </b-form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                username: "",
+                password: "",
+            },
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { BootstrapVue } from "bootstrap-vue";
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount("#app");

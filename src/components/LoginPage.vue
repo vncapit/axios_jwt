0d5a9a680031069c23b68b34ee272b56522f9190
cap <vncapit@gmail.com>
2023-04-21 22:03:02 +0800

    dev branch init

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,31 +1,78 @@
 <template>
-    <div>
-        <b-form @submit="onSubmit" class="login-form">
-            <b-form-group id="username-group" label="Username:" label-for="username">
-                <b-form-input id="username-input" v-model="form.username" type="text" placeholder="Username" required>
-                </b-form-input>
-            </b-form-group>
-            <b-form-group class="mt-3" id="password-group" label="Password:" label-for="password">
-                <b-form-input id="password-input" v-model="form.password" type="password" placeholder="Password" required>
-                </b-form-input>
-            </b-form-group>
-            <b-container class="mt-3">
-                <b-row align-h="around">
-                    <b-col cols="4">
-                        <b-button type="submit" variant="primary">Login</b-button>
-                    </b-col>
-                    <b-col cols="4">
-                        <b-button type="reset" variant="danger">Reset</b-button>
-                    </b-col>
-                </b-row>
-            </b-container>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 mx-auto">
+                <div class="card">
+                    <div class="card-header">Login</div>
+                    <div class="card-body">
+                        <form action="">
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input
+                                    id="username"
+                                    type="text"
+                                    class="form-control"
+                                    v-model="form.username"
+                                />
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input
+                                    id="password"
+                                    type="password"
+                                    class="form-control"
+                                    v-model="form.password"
+                                />
+                            </div>
 
+                            <button
+                                type="submit"
+                                @click="onSubmit"
+                                class="btn btn-outline-success btn-block mt-4"
+                            >
+                                Login
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-        </b-form>
+        <div
+            class="position-fixed bottom-0 right-0 p-3"
+            style="z-index: 5; right: 0; top: 0"
+        >
+            <div
+                id="liveToast"
+                class="toast"
+                role="alert"
+                aria-live="assertive"
+                aria-atomic="true"
+                data-delay="2000"
+            >
+                <div class="toast-header">
+                    <strong class="mr-auto">xxxx</strong>
+                    <small>11 mins ago</small>
+                    <button
+                        type="button"
+                        class="ml-2 mb-1 close"
+                        data-dismiss="toast"
+                        aria-label="Close"
+                    >
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="toast-body">
+                    Hello, world! This is a toast message.
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+import $ from "jquery";
 export default {
     data() {
         return {
@@ -33,7 +80,60 @@ export default {
                 username: "",
                 password: "",
             },
+            loginData: {
+                isLoading: false,
+                success: false,
+                token: "",
+            },
+            toast: {
+                title: "",
+                content: "",
+            },
         };
+    },
+    methods: {
+        onSubmit(e) {
+            e.preventDefault();
+            this.showToast();
+            // this.login();
+        },
+
+        async login() {
+            this.isLoading = true;
+            try {
+                const res = await axios.post(
+                    "http://oa.pbe.com/api/v1/auth/login",
+                    this.form
+                );
+
+                this.loginData.isLoading = false;
+                if (res.status == 200) {
+                    const data = res.data;
+                    if (data.code == 200) {
+                        this.loginData.success = true;
+                        this.loginData.token = data.data;
+                        this.showToast("success", 200);
+                        return;
+                    }
+                    this.loginData.success = false;
+                    this.loginData.token = "";
+                    this.showToast("fail", data.code);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        showToast(title, content) {
+            if (title == "success") {
+                this.toast.title = "Login success";
+                this.toast.content = 200;
+            } else {
+                this.toast.title = "Login failed";
+                this.toast.content = content;
+            }
+            $(".toast").toast("show");
+        },
     },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
-import { BootstrapVue } from "bootstrap-vue";
+// import { BootstrapVue } from "bootstrap-vue";
 // Import Bootstrap and BootstrapVue CSS files (order is important)
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap-vue/dist/bootstrap-vue.css";
 // Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue);
+
+// Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
+/* eslint-disable no-new */
 new Vue({
     render: (h) => h(App),
 }).$mount("#app");

052093d5b1c5843a03e4fe861ec45c660f118608
unknown <vncapit@gmail.com>
2023-04-25 23:06:11 +0800

    done carousel

--- src/components/DashboardPage.vue
+++ src/components/DashboardPage.vue
@@ -9,84 +9,39 @@
         </nav>
 
         <div class="container">
-            <div
-                id="carouselExampleIndicators"
-                class="carousel slide"
-                data-ride="carousel"
-            >
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                 <ol class="carousel-indicators">
-                    <li
-                        data-target="#carouselExampleIndicators"
-                        data-slide-to="0"
-                        class="active"
-                    ></li>
-                    <li
-                        data-target="#carouselExampleIndicators"
-                        data-slide-to="1"
-                    ></li>
-                    <li
-                        data-target="#carouselExampleIndicators"
-                        data-slide-to="2"
-                    ></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                 </ol>
-                <div
-                    class="carousel-inner d-flex"
-                    style="justify-content: start"
-                >
-                    <div class="carousel-item active">
-                        <img
-                            class="img d-block mx-auto"
-                            :src="
-                                `http://172.18.0.246:90/` +
-                                top10Activities[0]?.resources[0]?.path
-                            "
-                            alt="logo.png"
-                        />
+                <div class="carousel-inner d-flex" style="justify-content: start">
+                    <div class="carousel-item active item-style">
+                        <img class="d-block mx-auto" :src="`http://172.18.0.246:90/` +
+                            top10Activities[0]?.resources[0]?.path
+                            " alt="logo.png" />
                         <div class="carousel-caption d-none d-md-block">
-                            <h5>{{ top10Activities[0]?.title }}</h5>
+                            <span>{{ top10Activities[0]?.title }}</span>
                         </div>
                     </div>
 
-                    <div
-                        class="carousel-item"
-                        v-for="activity in top9Activities"
-                        :key="activity?.id"
-                    >
-                        <img
-                            :src="
-                                `http://172.18.0.246:90/` +
-                                activity?.resources[0]?.path
-                            "
-                            class="d-block mx-auto img"
-                            alt="../assets/logo.png"
-                        />
+                    <div class="carousel-item item-style" v-for="activity in top9Activities" :key="activity?.id">
+                        <img :src="`http://172.18.0.246:90/` +
+                            activity?.resources[0]?.path
+                            " class="d-block mx-auto" alt="../assets/logo.png" />
                         <div class="carousel-caption d-none d-md-block">
-                            <h5>{{ activity?.title }}</h5>
+                            <span>{{ activity?.title }}</span>
                         </div>
                     </div>
                 </div>
-                <button
-                    class="carousel-control-prev"
-                    type="button"
-                    data-target="#carouselExampleIndicators"
-                    data-slide="prev"
-                >
-                    <span
-                        class="carousel-control-prev-icon"
-                        aria-hidden="true"
-                    ></span>
+                <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators"
+                    data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
                 </button>
-                <button
-                    class="carousel-control-next"
-                    type="button"
-                    data-target="#carouselExampleIndicators"
-                    data-slide="next"
-                >
-                    <span
-                        class="carousel-control-next-icon"
-                        aria-hidden="true"
-                    ></span>
+                <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators"
+                    data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="sr-only">Next</span>
                 </button>
             </div>
@@ -133,10 +88,24 @@ export default {
     width: 25px;
     height: 25px;
     margin-right: 10px;
+    border-radius: 50%;
 }
 
-.img {
+.carousel-item img {
     max-width: 1000px;
     max-height: 350px;
+}
+
+.item-style {
+    height: 350px;
+    background-color: rgb(235, 230, 234);
+}
+
+.carousel-caption span {
+    color: rgb(255, 255, 255);
+    background-color: rgba($color: #ffffff, $alpha: 0.3);
+    max-width: max-content;
+    padding: 5px;
+    border-radius: 3px;
 }
 </style>
